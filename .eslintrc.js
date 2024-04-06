a1ec71e8a046248cc17113976471903932d4ae04
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-04-06 15:37:19 +0530

    feat: add no-floating-promise rule to eslint✅

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     "no-console": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
     "no-empty-function": "warn",
-    "@typescript-eslint/no-unnecessary-condition": "error"
+    "@typescript-eslint/no-unnecessary-condition": "error",
+    "@typescript-eslint/no-floating-promises": "error"
   },
 };
