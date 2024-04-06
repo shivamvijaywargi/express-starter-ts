869718e0f11380995810a25331dedd8f07ca8b3c
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-04-06 15:30:33 +0530

    feat: handle, uncaught exceptions, unhandled promise rejections

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,28 @@ const PORT = parseInt(CONFIG.PORT || "5502", 10);
 
 const startServer = (PORT: number) => {
   try {
-    app.listen(PORT, () => {
+    // Handling uncaught exceptions
+    process.on("uncaughtException", (err) => {
+      logger.error(`Error: ${err.message}`);
+      logger.error("Shutting down the server due to uncaught exceptions");
+
+      process.exit(1);
+    });
+
+    const server = app.listen(PORT, () => {
       logger.info(`App is running at http://localhost:${PORT}`);
+    });
+
+    // Handling Unhandled exceptions
+    process.on("unhandledRejection", (err: Error) => {
+      logger.error(`Error: ${err.message}`);
+      logger.error(
+        "Shutting down the server due to unhandled promise rejections",
+      );
+
+      server.close(() => {
+        process.exit(1);
+      });
     });
   } catch (err: unknown) {
     if (err instanceof Error) {
